d665dc5bbc59b57017662de158e714fb61964fdd
Ben Radler <benradler@me.com>
2014-06-29 22:39:35 -0700

    Bitly integration working

--- app.rb
+++ app.rb
@@ -17,17 +17,26 @@ class App < Sinatra::Base
 
     # take the link, and generate N number of variations, where N = multiplier,
     # taking into account the start value, S
-    url         = params['url']
-    multiplier  = params['multiplier'].to_i
-    start_value = params['start_value'].to_i
+    url            = params['url']
+    multiplier     = params['multiplier'].to_i
+    start_value    = params['start_value'].to_i
+    tweet_template = params['tweet_template']
 
-    range = Range.new(start_value, start_value + multiplier, true)
-    array = Array.new
-    range.each { |i| array << "#{url}##{i}" }
+    range          = Range.new(start_value, start_value + multiplier, true)
+    numbered_urls  = Array.new
+    range.each { |i| numbered_urls << "#{url}##{i}" }
+
     # for each generated URL use bitly to shorten
-    # BitlyClient.new
-
+    # { 0: { long_url: "http://foo.com#1", short_url: "http://bit.ly/whatever" } }
     # stick each shortened URL into the tweet template
+    client       = BitlyClient.new.client
+    results_hash = Hash.new
+
+    numbered_urls.each_with_index do |url, i|
+      results = client.shorten(url)
+      tweet   = tweet_template.gsub('{link}', results.short_url)
+      results_hash[i] = { long_url: results.long_url, tweet: tweet }
+    end
 
     # build a CSV and send_file it to the user
     # results must be a CSV of
@@ -36,16 +45,16 @@ class App < Sinatra::Base
   end
 end
 
+
 class BitlyClient
+  attr_reader :client
+
   def initialize
     Bitly.configure do |config|
       config.api_version = 3
       config.login   = ENV['username'] || Settings.username
       config.api_key = ENV['api_key']  || Settings.api_key
     end
-  end
-
-  def shorten
-
+    @client = Bitly.client
   end
 end
